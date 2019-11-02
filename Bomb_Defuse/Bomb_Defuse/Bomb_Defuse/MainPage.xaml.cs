52d2cda8aab8a8420389d9e211c088ef1b629cf5
Tobias Bühler <tobias.buehler@t-online.de>
2019-11-02 22:24:13 +0100

    - added demo project

---
+++ Bomb_Defuse/Bomb_Defuse/Bomb_Defuse/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bomb_Defuse
+{
+    public partial class MainPage : ContentPage
+    {
+        static string bombCode = "";
+        static int clickCount = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            generateBombCode();
+        }
+
+        async void Btn_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Text.Equals(bombCode))
+            {
+                clickCount++;
+                lbl_defCode.Text = this.lbl_defCode.Text + bombCode;
+                generateBombCode();
+                if (clickCount == 3)
+                {
+                    await DisplayAlert("BOMB DEFUSED", "You got the code right", "RETRY");
+                    lbl_defCode.Text = "Code: ";
+                    clickCount = 0;
+                    generateBombCode();
+                }
+            }
+            else {
+                await DisplayAlert("GAME OVER", "This was the wrong code", "RETRY");
+                lbl_defCode.Text = "Code: ";
+                clickCount = 0;
+                generateBombCode();
+            }
+
+        }
+        private void generateBombCode()
+        {
+                bombCode = new Random().Next(0, 2).ToString();
+                //this.lbl_defCode.Text = "Code: " + bombCode;
+        }
+    }
+}
